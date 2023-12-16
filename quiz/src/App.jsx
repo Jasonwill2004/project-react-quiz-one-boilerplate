9ce265ac3df5fd8e401869c21d7f009dcc4d8904
Jasonwill2004 <jason.william@kalvium.community>
2023-12-16 09:24:42 +0530

    mass

---
+++ quiz/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import HomeComponent from './Components/HomeComponents.jsx'
+import QuizComponent from './Components/QuizComponent.jsx'
+import ResultComponent from './Components/ResutComponent.jsx'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='App'>
+      <HomeComponent/>
+      <QuizComponent/>
+      <ResultComponent/>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ quiz/src/Components/HomeComponents.jsx
@@ -0,0 +1,10 @@
+import './HomeComponents.css'
+
+let HomeComponent=()=>(
+    <div className="Home">
+        <div className='title'><h1>Quiz App</h1></div>
+        <button>Play Now</button>
+    </div>
+)
+
+export default HomeComponent
